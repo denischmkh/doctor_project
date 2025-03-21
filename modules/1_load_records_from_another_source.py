4354391c46424b041b45f77e9e67e506735a9d12
denischmkh <avraam.l2004@gmail.com>
2025-03-21 12:29:47 +0200

    new_parser

--- modules/1_load_records_from_another_source.py
+++ modules/1_load_records_from_another_source.py
@@ -1,5 +1,8 @@
 import csv
 import os
+
+from django.db import DataError
+
 from load_django import *
 from parser_app.models import *
 from pathlib import Path
@@ -15,20 +18,24 @@ for file in files:
     with open(file_path, mode='r', encoding='utf-8') as f:
         reader = csv.DictReader(f)
         for row in reader:
-            new_doctor = Doctor.objects.get_or_create(
-                name=row.get('doctor_name', None) if row.get('doctor_name') != "NULL" else None,
-                profile_url=row.get('doctor_link', None) if row.get('doctor_link') != "NULL" else None,
-                address=row.get('address', None) if row.get('address') != "NULL" else None,
-                phone=row.get('phone')[3:-2] if row.get('phone') != "NULL" else None,
-                fax=row.get('fax') if row.get('fax') != "NULL" else None,
-                site_url=row.get('website') if row.get('website') != "NULL" else None,
-                email=row.get('doctor_email')[3:-2].split(",")[0] if row.get('doctor_email') != "NULL" else None,
-                instagram=row.get('doctor_instagram', None)[1:-1] if row.get('doctor_instagram') != "NULL" else None,
-                facebook=row.get('doctor_facebook', None)[1:-1] if row.get('doctor_facebook') != "NULL" else None,
-                twitter=row.get('doctor_twitter', None)[1:-1] if row.get('doctor_twitter') != "NULL" else None,
-                linkedin=row.get('doctor_linkedin', None)[1:-1] if row.get('doctor_linkedin') != "NULL" else None,
-                youtube=row.get('doctor_youtube', None)[1:-1] if row.get('doctor_youtube') != "NULL" else None,
-            )
+            try:
+                new_doctor = Doctor.objects.get_or_create(
+                    name=row.get('doctor_name', None) if row.get('doctor_name') != "NULL" else None,
+                    profile_url=row.get('doctor_link', None) if row.get('doctor_link') != "NULL" else None,
+                    address=row.get('address', None) if row.get('address') != "NULL" else None,
+                    phone=row.get('phone')[3:-2] if row.get('phone') != "NULL" else None,
+                    fax=row.get('fax') if row.get('fax') != "NULL" else None,
+                    site_url=row.get('website') if row.get('website') != "NULL" else None,
+                    email=row.get('doctor_email')[3:-2].split(",")[0] if row.get('doctor_email') != "NULL" else None,
+                    instagram=row.get('doctor_instagram', None)[1:-1] if row.get('doctor_instagram') != "NULL" else None,
+                    facebook=row.get('doctor_facebook', None)[1:-1] if row.get('doctor_facebook') != "NULL" else None,
+                    twitter=row.get('doctor_twitter', None)[1:-1] if row.get('doctor_twitter') != "NULL" else None,
+                    linkedin=row.get('doctor_linkedin', None)[1:-1] if row.get('doctor_linkedin') != "NULL" else None,
+                    youtube=row.get('doctor_youtube', None)[1:-1] if row.get('doctor_youtube') != "NULL" else None,
+                )
+            except DataError:
+                "Неподходящее значение"
+                continue
 
             # Обработка specialties
             specialties = []
