d8be0705e078a51df7e9ed022d7fd02276e71d5c
denischmkh <avraam.l2004@gmail.com>
2025-03-28 15:54:08 +0200

    remake profile

--- parser_app/views.py
+++ parser_app/views.py
@@ -94,8 +94,8 @@ def doctor_profile(request, id: int):
     specialisations = doctor.specialisations.all()
     memberships = doctor.memberships.all()
     awards = doctor.awards.all()
+    print(awards)
     main_speciality = specialisations[0]
-    print(main_speciality)
     return render(request, 'doctor-profile.html', {'doctor': doctor,
                                                    'awards_count': awards_count,
                                                    'working_from': min(years_experience) if years_experience else None,
