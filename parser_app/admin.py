65505c7350048d887d3b508bc460c6a5f04b2c21
denischmkh <avraam.l2004@gmail.com>
2025-04-01 16:35:34 +0300

    develop clinics

--- parser_app/admin.py
+++ parser_app/admin.py
@@ -1,9 +1,77 @@
 from django.contrib import admin
-from .models import Clinic, Doctor
+from .models import (
+    Doctor, Clinic, Language, Hospital, Specialisation, Education, WorkExperience,
+    Apprenticeship, Publication, Research, Award, Competence, Membership
+)
 
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "phone")
+    search_fields = ("name", "email", "phone")
+    list_filter = ("gender", "city")
+    filter_horizontal = (
+        "clinic", "languages", "hospitals", "specialisations", "educations",
+        "work_experience", "apprenticeships", "publications", "researches",
+        "awards", "competences", "memberships"
+    )
 
-admin.site.register(Doctor)
-admin.site.register(Clinic)
+@admin.register(Clinic)
+class ClinicAdmin(admin.ModelAdmin):
+    list_display = ("title", "location")
+    search_fields = ("title", "location")
 
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
 
+@admin.register(Hospital)
+class HospitalAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+@admin.register(Specialisation)
+class SpecialisationAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+@admin.register(Education)
+class EducationAdmin(admin.ModelAdmin):
+    list_display = ("info", "year")
+    search_fields = ("info", "year")
+
+@admin.register(WorkExperience)
+class WorkExperienceAdmin(admin.ModelAdmin):
+    list_display = ("info", "year")
+    search_fields = ("info", "year")
+
+@admin.register(Apprenticeship)
+class ApprenticeshipAdmin(admin.ModelAdmin):
+    list_display = ("info", "year")
+    search_fields = ("info", "year")
+
+@admin.register(Publication)
+class PublicationAdmin(admin.ModelAdmin):
+    list_display = ("title", "year")
+    search_fields = ("title", "year")
+
+@admin.register(Research)
+class ResearchAdmin(admin.ModelAdmin):
+    list_display = ("title", "year")
+    search_fields = ("title", "year")
+
+@admin.register(Award)
+class AwardAdmin(admin.ModelAdmin):
+    list_display = ("name", "year")
+    search_fields = ("name", "year")
+
+@admin.register(Competence)
+class CompetenceAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+@admin.register(Membership)
+class MembershipAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
 
