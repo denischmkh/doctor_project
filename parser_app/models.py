a1de2e575d13732b9610e4ac729320fa7bf0e643
denischmkh <avraam.l2004@gmail.com>
2025-04-01 16:34:06 +0300

    develop clinics

--- parser_app/models.py
+++ parser_app/models.py
@@ -1,127 +1,76 @@
-from django.contrib.postgres.fields import ArrayField
-from django.db import models
-from django.db.models import IntegerField, CharField, TextField, Model, ForeignKey, URLField, JSONField, ManyToManyField
+from django.contrib import admin
+from .models import (
+    Doctor, Clinic, Language, Hospital, Specialisation, Education, WorkExperience,
+    Apprenticeship, Publication, Research, Award, Competence, Membership
+)
 
-class Language(models.Model):
-    name = TextField(null=True)
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "phone")
+    search_fields = ("name", "email", "phone")
+    list_filter = ("gender", "city")
+    filter_horizontal = (
+        "clinic", "languages", "hospitals", "specialisations", "educations",
+        "work_experience", "apprenticeships", "publications", "researches",
+        "awards", "competences", "memberships"
+    )
 
-    def __str__(self):
-        return self.name
+@admin.register(Clinic)
+class ClinicAdmin(admin.ModelAdmin):
+    list_display = ("title", "location")
+    search_fields = ("title", "location")
 
-class Hospital(models.Model):
-    name = TextField(null=True)
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
 
-    def __str__(self):
-        return self.name
+@admin.register(Hospital)
+class HospitalAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
 
-class Specialisation(models.Model):
-    name = TextField(null=True)
+@admin.register(Specialisation)
+class SpecialisationAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
 
-    def __str__(self):
-        return self.name
+@admin.register(Education)
+class EducationAdmin(admin.ModelAdmin):
+    list_display = ("info", "year")
+    search_fields = ("info", "year")
 
-class Education(models.Model):
-    info = TextField(null=True)
-    year = CharField(null=True)
+@admin.register(WorkExperience)
+class WorkExperienceAdmin(admin.ModelAdmin):
+    list_display = ("info", "year")
+    search_fields = ("info", "year")
 
-    def __str__(self):
-        return f"{self.info} ({self.year if self.year else 'N/A'})"
+@admin.register(Apprenticeship)
+class ApprenticeshipAdmin(admin.ModelAdmin):
+    list_display = ("info", "year")
+    search_fields = ("info", "year")
 
-class WorkExperience(models.Model):
-    info = TextField()
-    year = CharField(null=True)
+@admin.register(Publication)
+class PublicationAdmin(admin.ModelAdmin):
+    list_display = ("title", "year")
+    search_fields = ("title", "year")
 
-    def __str__(self):
-        return f"{self.info} ({self.year if self.year else 'N/A'})"
+@admin.register(Research)
+class ResearchAdmin(admin.ModelAdmin):
+    list_display = ("title", "year")
+    search_fields = ("title", "year")
 
-class Apprenticeship(models.Model):
-    info = TextField()
-    year = CharField(null=True)
+@admin.register(Award)
+class AwardAdmin(admin.ModelAdmin):
+    list_display = ("name", "year")
+    search_fields = ("name", "year")
 
-    def __str__(self):
-        return f"{self.info} ({self.year if self.year else 'N/A'})"
+@admin.register(Competence)
+class CompetenceAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
 
-class Publication(models.Model):
-    title = TextField(null=True)
-    year = CharField(null=True)
-
-    def __str__(self):
-        return f"{self.title} ({self.year if self.year else 'N/A'})"
-
-class Research(models.Model):
-    title = TextField(null=True)
-    year = CharField(null=True)
-
-    def __str__(self):
-        return f"{self.title} ({self.year if self.year else 'N/A'})"
-
-class Award(models.Model):
-    name = TextField(null=True)
-    year = CharField(null=True)
-
-    def __str__(self):
-        return f"{self.name} ({self.year if self.year else 'N/A'})"
-
-class Competence(models.Model):
-    name = TextField(null=True)
-
-    def __str__(self):
-        return self.name
-
-class Membership(models.Model):
-    name = TextField(null=True)
-
-    def __str__(self):
-        return self.name
-
-
-class Clinic(models.Model):
-    title = CharField(max_length=255, null=True)
-    location = CharField(max_length=255, null=True)
-    image = URLField(null=True)
-
-    def __str__(self):
-        return self.title
-
-
-class Doctor(models.Model):
-    name = models.TextField(null=True)
-    clinic = models.ManyToManyField(Clinic, blank=True)
-    description = models.TextField(null=True)
-    profile_url = models.URLField(null=True)
-    phone = models.TextField(null=True)
-    email = models.TextField(null=True)
-    vcard_url = models.URLField(null=True)
-    cv_url = models.URLField(null=True)
-    photo_url = models.URLField(null=True)
-
-
-    gender = models.CharField(null=True)
-    city = models.CharField(null=True)
-
-
-    address = models.TextField(null=True)
-    fax = models.TextField(null=True)
-    instagram = models.TextField(null=True)
-    facebook = models.TextField(null=True)
-    twitter = models.TextField(null=True)
-    linkedin = models.TextField(null=True)
-    youtube = models.TextField(null=True)
-
-    languages = models.ManyToManyField(Language, blank=True)
-    hospitals = models.ManyToManyField(Hospital, blank=True)
-    specialisations = models.ManyToManyField(Specialisation, blank=True)
-    educations = models.ManyToManyField(Education, blank=True)
-    work_experience = models.ManyToManyField(WorkExperience, blank=True)
-    apprenticeships = models.ManyToManyField(Apprenticeship, blank=True)
-    publications = models.ManyToManyField(Publication, blank=True)
-    researches = models.ManyToManyField(Research, blank=True)
-    awards = models.ManyToManyField(Award, blank=True)
-    competences = models.ManyToManyField(Competence, blank=True)
-    memberships = models.ManyToManyField(Membership, blank=True)
-
-    site_url = models.TextField(null=True)
-    media_urls = models.JSONField(null=True, blank=True)
-
-    def __str__(self):
-        return self.name
+@admin.register(Membership)
+class MembershipAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
